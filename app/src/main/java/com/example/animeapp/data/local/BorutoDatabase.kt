bfdb686fbfc19231d1219ce034b5762611d02a98
SkanderThabet <skander.thabet@esprit.tn>
2022-08-29 00:51:17 +0100

    Created Hero remote key dao

--- app/src/main/java/com/example/animeapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/example/animeapp/data/local/BorutoDatabase.kt
@@ -2,10 +2,14 @@ package com.example.animeapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.animeapp.data.local.dao.HeroDao
+import com.example.animeapp.data.local.dao.HeroRemoteKeyDao
 import com.example.animeapp.domain.model.Hero
+import com.example.animeapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
 
 abstract class BorutoDatabase : RoomDatabase() {
-    abstract fun heroDao()
+    abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDoa(): HeroRemoteKeyDao
 }
---
+++ app/src/main/java/com/example/animeapp/domain/model/HeroRemoteKey.kt
@@ -0,0 +1,13 @@
+package com.example.animeapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.animeapp.util.Constants.HERO_REMOTE_KEY_DATABASE_TABLE
+
+@Entity(tableName = HERO_REMOTE_KEY_DATABASE_TABLE)
+data class HeroRemoteKey(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val prevPage: Int?,
+    val nextPage: Int?,
+)
