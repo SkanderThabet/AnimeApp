63a0ff1aef6c384f338873d24453b50efcece3e5
SkanderThabet <skander.thabet@esprit.tn>
2022-08-29 03:15:58 +0100

    Designed the splash screen with Animation - 3/3

--- app/src/main/java/com/example/animeapp/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/splash/SplashScreen.kt
@@ -1,13 +1,18 @@
 package com.example.animeapp.presentation.screens.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -20,11 +25,22 @@ import com.example.animeapp.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+    val degrees = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+
+    }
+    Splash(degrees = degrees.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees: Float) {
     if (isSystemInDarkTheme()) {
         Box(
             modifier = Modifier
@@ -35,6 +51,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -49,6 +66,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.rotate(degrees = degrees),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -60,5 +78,5 @@ fun Splash() {
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    Splash()
+    Splash(degrees = 360f)
 }
