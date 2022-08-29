9eccfd8c1e648300e142d210afeb6c880922a4bf
SkanderThabet <skander.thabet@esprit.tn>
2022-08-29 02:40:23 +0100

    Designed the splash screen - 1/3

--- app/src/main/java/com/example/animeapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/animeapp/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.animeapp.presentation.screens.splash.SplashScreen
 import com.example.animeapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
@@ -15,7 +16,7 @@ fun SetupNavGraph(navController: NavHostController) {
         startDestination = Screens.Splash.route,
     ) {
         composable(route = Screens.Splash.route) {
-
+            SplashScreen(navController = navController)
         }
         composable(route = Screens.Welcome.route) {
 
