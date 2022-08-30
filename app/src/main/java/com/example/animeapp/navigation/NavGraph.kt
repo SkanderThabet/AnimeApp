5cea71d755db187ef5b6acade22521a45cf50f01
SkanderThabet <skander.thabet@esprit.tn>
2022-08-30 02:08:40 +0100

    Created welcome screen

--- app/src/main/java/com/example/animeapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/animeapp/navigation/NavGraph.kt
@@ -7,19 +7,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.animeapp.presentation.screens.splash.SplashScreen
+import com.example.animeapp.presentation.screens.welcome.WelcomeScreen
 import com.example.animeapp.util.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screens.Splash.route,
+        startDestination = Screens.Welcome.route,
     ) {
         composable(route = Screens.Splash.route) {
             SplashScreen(navController = navController)
         }
         composable(route = Screens.Welcome.route) {
-
+            WelcomeScreen(navController = navController)
         }
         composable(
             route = Screens.Details.route,
---
+++ app/src/main/java/com/example/animeapp/presentation/screens/welcome/WelcomeScreen.kt
@@ -0,0 +1,149 @@
+package com.example.animeapp.presentation.screens.welcome
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.example.animeapp.R
+import com.example.animeapp.domain.model.OnBoardingPage
+import com.example.animeapp.ui.theme.*
+import com.example.animeapp.util.Constants.ON_BOARDING_PAGE_COUNT
+import com.google.accompanist.pager.*
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third,
+    )
+
+    val pagerState = rememberPagerState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.welcomeScreenBackgroundColor)
+
+    ) {
+        HorizontalPager(
+            modifier = Modifier.weight(10f),
+            count = ON_BOARDING_PAGE_COUNT,
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { index ->
+            PagerScreen(onBoardingPage = pages[index])
+        }
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterHorizontally),
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colors.activeIndicatorColor,
+            inactiveColor = MaterialTheme.colors.inactiveIndicatorColor,
+            indicatorWidth = PAGING_INDICATOR_WIDTH,
+            spacing = PAGING_INDICATOR_SPACING
+        )
+        FinishButton(
+            modifier = Modifier
+                .weight(1f),
+            pagerState = pagerState
+        ) {
+
+        }
+    }
+
+
+}
+
+@Composable
+fun PagerScreen(onBoardingPage: OnBoardingPage) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            contentDescription = stringResource(R.string.onboard_image),
+            painter = painterResource(id = onBoardingPage.image),
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = onBoardingPage.title,
+            color = MaterialTheme.colors.titleColor,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = EXTRA_LARGE_PADDING)
+                .padding(top = SMALL_PADDING),
+            text = onBoardingPage.description,
+            color = MaterialTheme.colors.descriptionColor,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = EXTRA_LARGE_PADDING),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxWidth(),
+            visible = pagerState.currentPage == 2
+        ) {
+            Button(
+                onClick = onClick,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.buttonBackgroundColor,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = stringResource(R.string.finish_button))
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun FirstOnBoardScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.First)
+    }
+
+}
