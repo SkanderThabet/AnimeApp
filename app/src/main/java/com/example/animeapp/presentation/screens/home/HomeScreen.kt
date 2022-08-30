fe310b1cc9286c4b0b9dd68af77567a253ed8752
SkanderThabet <skander.thabet@esprit.tn>
2022-08-30 22:36:27 +0100

    Home Screen - Design Top Bar

--- app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/animeapp/presentation/screens/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.animeapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
@@ -7,5 +8,13 @@ import androidx.navigation.NavHostController
 fun HomeScreen(
     navController: NavHostController
 ) {
+    Scaffold(
+        topBar = {
+            HomeTopBar {
+                
+            }
+        }
+    ) {
 
+    }
 }
---
+++ app/src/main/java/com/example/animeapp/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,39 @@
+package com.example.animeapp.presentation.screens.home
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.animeapp.R
+import com.example.animeapp.ui.theme.topAppBarBackgroundColor
+import com.example.animeapp.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.explore_top_bar),
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+
+    )
+}
+
+@Preview
+@Composable
+fun HomeTopBarPreview() {
+    HomeTopBar(onSearchClicked = {})
+}
