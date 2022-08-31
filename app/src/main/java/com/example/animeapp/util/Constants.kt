062e6308df62243349fc34d20f92ca8f2df9e30b
SkanderThabet <skander.thabet@esprit.tn>
2022-08-31 03:12:14 +0100

    Add Network Module

--- app/src/main/java/com/example/animeapp/util/Constants.kt
+++ app/src/main/java/com/example/animeapp/util/Constants.kt
@@ -12,4 +12,6 @@ object Constants {
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_KEY = "on_boarding_completed"
 
+    const val BASE_URL = "http://10.0.2.2:8080"
+
 }
